bc8a7af700372efdfd057a3fe3cb645dc079ac38
CaioCDJ <caiodjesus1@hotmail.com>
2022-07-04 14:23:30 -0300

    Controlled component

--- ex/index.jsx
+++ ex/index.jsx
@@ -12,15 +12,26 @@ import ReactDOM from 'react-dom';
 // ex02
 // import SilvaFamily from './ex02Family.jsx';
 
+/* ex03
 import Family from './ex03Family.jsx';
 import Member from './ex02.jsx'
 
-ReactDOM.render(
-  <Family lastName="Silva">
+ <Family lastName="Silva">
     <Member name="guilherme"/>
     <Member name="Rafael"/>
     <Member name="Julia"/>
-  </Family>,
+  </Family>
+  */
+/* ex04Class
+import ClassComponent from './ex04Class.jsx';
+<ClassComponent 
+      label="Contador"
+      initialValue={10} />
+*/
+
+import Field from './ex05Field';
+ReactDOM.render(
+  <Field initialValue="Test"/>,
 document.getElementById('app'));
 
 
